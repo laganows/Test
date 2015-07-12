a1dfd48af59e2dc00bc794da28d87f60ddbdd4ea
Grzegorz Laganowski <g.laganowski94@gmail.com>
2015-07-12 18:11:43 +0200

    Load dataset for chart from CSV file

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var globalDx = 1
-var globalDy = 1
+var globalDx = 1;
+var globalDy = 1;
+var globalArary = [];
 
 function updateX() {
-    var dx = document.getElementById("dx").value
-    globalDx  = dx
+    var dx = document.getElementById("dx").value;
+    globalDx  = dx;
     updateChart()
 }
 
 function updateY() {
-    var dy = document.getElementById("dy").value
-    globalDy  = dy
+    var dy = document.getElementById("dy").value;
+    globalDy  = dy;
     updateChart()
 }
 
@@ -53,81 +54,51 @@ function updateChart() {
                 borderWidth: 0
             },
             series: [{
-                data: [[5, 2], [6, 3], [8, 2], [9, 4]]
+                data: globalArary
+                //data: [[5, 2], [6, 3], [8, 2], [9, 4]]
             }]
         });
     });
 }
 
-/*
-function zad1() {
-    $(document).ready(function() {
-        var dx = 2
-        var dy = 1
-
-        $("input#submitButtonDx").click(function(){
-            var valueToCheck = document.getElementById("dx").value;
-            var naturalNumbersPattern = /^[1-9][0-9]*$/
-            var realNumbersPattern = /^[0-9]+[.][0-9]*$/
-            if (realNumbersPattern.test(valueToCheck) || naturalNumbersPattern.test(valueToCheck)) {
-                dx = valueToCheck
-                alert("Good number");
-            } else {
-                alert("Wrong number");
-                return
-            }
-
-        });
-
-        $("input#submitButtonDy").click(function(){
-            alert("button");
-        });
-
-
-
-        $(function () {
-            $('#container').highcharts({
-                title: {
-                    text: 'Chart',
-                    x: -20 //center
-                },
-                subtitle: {
-                    text: 'Source: Example data',
-                    x: -20
-                },
-                xAxis: {
-                    title: {
-                        text: 'x'
-                    },
-                    tickInterval: document.getElementById("dx").value,
-                    plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: '#808080'
-                    }]
-                },
-                yAxis: {
-                    title: {
-                        text: 'y'
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: '#808080'
-                    }]
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle',
-                    borderWidth: 0
-                },
-                series: [{
-                    data: [[5, 2], [6, 3], [8, 2], [9, 4]]
-                }]
-            });
-        });
-
-    });
+function toArray(csv) {
+    $.csv.toArrays(csv);
 }
-*/
+
+
+function readTextFile(file) {
+    file = document.getElementById("fileName").value;
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+
+                processData(allText);
+            }
+        }
+    };
+    rawFile.send(null);
+}
+
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = new Array();
+
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+            lines[i-1] = new Array(2)
+            lines[i-1][0] = Number(data[0])
+            lines[i-1][1] = Number(data[1])
+        }
+    }
+    globalArary = lines
+    updateChart()
+    alert('Sucess with load the file');
+}
+
